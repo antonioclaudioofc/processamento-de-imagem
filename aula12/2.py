38599bfc1a4ba9b4aa0e555a8bc1ed0c31260b50
Antonio Claudio <claudio1419teixeira2020@gmail.com>
2024-08-05 08:55:33 -0300

    fix: aula12

--- aula12/2.py
+++ aula12/2.py
@@ -6,16 +6,25 @@ kernel = np.ones((3,3),np.uint8)
 
 img = cv2.imread('moedas.jpg')
 
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-ret,thresh = cv2.threshold(img_gray,200,255,cv2.THRESH_BINARY_INV)
+_ ,mask = cv2.threshold(gray,240,255,cv2.THRESH_BINARY)
+mask_inv = cv2.bitwise_not(mask)
 
-closing = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+closing = cv2.morphologyEx(mask_inv, cv2.MORPH_CLOSE, kernel)
 
-canny = cv2.Canny(closing, 100, 300)
+canny = cv2.Canny(mask_inv, 100, 300)
 
-contours, hierarchy = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-cv2.drawContours(img, contours, -1, (0, 0, 255), 2)
+contours, _ = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(img, contours, -1, 127, 2)
+
+for contour in contours:
+    (x, y), radius = cv2.minEnclosingCircle(contour)
+    center = (int(x), int(y))
+    radius = int(radius)
+    cv2.circle(img, center, radius, (0, 255, 0), 2)
+    cv2.putText(img, f'R: {radius}', (center[0] - 10, center[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
+
 
 cv2.imshow('Contours', img)
 
